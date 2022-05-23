f54b0677de4a091d4d50d0baab2f3bbb1856c558
nikoolass <na.gutierrez@outlook.com>
2022-05-23 15:26:43 -0300

    incremental static generation agregado

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -97,7 +97,8 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: pokemons151.map( id => ({
       params: { id }
     })),
-    fallback: false
+    // fallback: false
+    fallback: "blocking"
   }
 }
 
@@ -105,10 +106,22 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
   const { id } = params as { id: string };
 
+  const pokemon = await getPokemonInfo(id)
+
+  if( !pokemon ) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+
   return {
     props: {
-      pokemon: await getPokemonInfo(id)
-    }
+      pokemon
+    },
+    revalidate: 86400, // 60 * 60 * 24 segundos
   }
 }
 
