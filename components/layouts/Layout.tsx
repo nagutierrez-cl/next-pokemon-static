e923d8414579bfd30445a042a0f1afdc316bd995
nikoolass <na.gutierrez@outlook.com>
2022-05-23 13:08:06 -0300

    og image arreglado

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -22,7 +22,7 @@ export const Layout: FC<Props>= ({ children, title }) => {
 
         <meta property="og:title" content={`Información sobre ${ title }`}/>
         <meta property="og:description" content={`Esta es la página sobre ${ title }`} />
-        <meta property="og:image" content={`http://${ origin }/img/banner.png`} />
+        <meta property="og:image" content={`${ origin }/img/banner.png`} />
       </Head>
 
       <NavBar />
--- utils/localFavorites.ts
+++ utils/localFavorites.ts
@@ -1,5 +1,4 @@
 const toggleFavorite = ( id: number ) => {
-  console.log('toggleFavorite');
 
   let favorites: number[] = JSON.parse( localStorage.getItem('favorites') || '[]' );
 
